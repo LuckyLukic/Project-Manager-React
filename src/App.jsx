cd904faf6cb4ba337e0604658e9feca34131b378
LuckyLukic <lucabjjiannice@gmail.com>
2025-11-11 23:13:02 +0100

    connecting buttons uplifting state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import Sidebar from "./components/Sidebar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  function handleClick() {
+    setButtonClicked(true);
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar></Sidebar>
-      <NewProject></NewProject>
+      <Sidebar onClick={handleClick}></Sidebar>
+      {buttonClicked ? (
+        <NewProject></NewProject>
+      ) : (
+        <NoProjectSelected onClick={handleClick}></NoProjectSelected>
+      )}
     </main>
   );
 }
